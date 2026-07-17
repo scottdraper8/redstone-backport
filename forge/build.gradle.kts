71613712e6ab2010cbd5ffe3859d651addb25efe
Scott Draper <admin@scottdraper.io>
2026-07-17 13:49:10 -0600

    Revert "Disable Forge parameter-name Error Prone checker"
    This reverts commit b308a696fba3c19e7b0b6538a028c8b8caa75fc0.

--- forge/build.gradle.kts
+++ forge/build.gradle.kts
@@ -154,7 +154,5 @@ tasks.withType<JavaCompile>().configureEach {
         disableWarningsInGeneratedCode.set(true)
         option("NullAway:AnnotatedPackages", "com.squinchmods.redstonebackport")
         error("NullAway")
-        // NeoForge's CI binary artifact path can expose duplicate synthetic parameter names.
-        disable("OverridingMethodInconsistentArgumentNamesChecker")
     }
 }
